f6f13058587e14fc75c448f9f3b68df247b33a1f
Fred <68167225+Frednm1@users.noreply.github.com>
2022-06-08 15:48:26 -0300

    a87

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas pessoais'),
+      ),
+      body: Column(
+        // crossAxisAlignment:
+        //     CrossAxisAlignment.start, //define o alinhamento do eixo Y
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('lista de transações'),
+          ),
+        ],
+      ),
+    );
+  }
+}
