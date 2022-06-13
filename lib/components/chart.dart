29cb99663a8fb4685137c515343fcad9a245e191
Fred <68167225+Frednm1@users.noreply.github.com>
2022-06-13 18:06:28 -0300

    a123

---
+++ lib/components/chart.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+import 'chart_bar.dart';
+
+class Chart extends StatelessWidget {
+  Chart(this.recentTransactions);
+
+  final List<Transaction> recentTransactions;
+
+  List<Map<String, dynamic>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      double totalSum = 0.0;
+      for (var i = 0; i < recentTransactions.length; i++) {
+        bool sameDay = recentTransactions[i].date.day == weekDay.day;
+        bool sameMonth = recentTransactions[i].date.month == weekDay.month;
+        bool sameYear = recentTransactions[i].date.year == weekDay.year;
+        if (sameDay && sameMonth && sameYear) {
+          totalSum += recentTransactions[i].value;
+        }
+      }
+      print(DateFormat.E().format(weekDay)[0]);
+      print(totalSum);
+      return {
+        'day': DateFormat.E().format(weekDay)[0],
+        'value': totalSum,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    groupedTransactions;
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: groupedTransactions.map((tr) {
+          return ChartBar(
+            tr['day'],
+            tr['value'],
+            0,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
--- lib/components/transactions_list.dart
+++ lib/components/transactions_list.dart
@@ -10,61 +10,80 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        //O map recebe uma função de callback(ou seja tem que ter retorno) como parâmetro e para cada instanciação ele roda essa função
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          final tr = transactions[index];
-          return Card(
-            child: Row(
-              children: <Widget>[
+      margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  'não tem nenhuma transação cadastrada',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
                 Container(
-                  //Containers possuem opções de estilo
-                  //Columns e Rows não possuem essas opções
-                  //Colunas sempre ocupam toda a altura disponivel e Rows a largura disponivel
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      //decoração de bordas
-                      border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  )),
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 10,
+                  margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
-                  child: Text(
-                    'R\$ ${tr.value.toStringAsFixed(2)}', //INTERPOLAÇÃO
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple,
-                    ),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      tr.title,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      DateFormat('d MMM y').format(tr.date), //importado do yaml
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
+                )
               ],
+            )
+          : ListView.builder(
+              //O map recebe uma função de callback(ou seja tem que ter retorno) como parâmetro e para cada instanciação ele roda essa função
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                final tr = transactions[index];
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        //Containers possuem opções de estilo
+                        //Columns e Rows não possuem essas opções
+                        //Colunas sempre ocupam toda a altura disponivel e Rows a largura disponivel
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            //decoração de bordas
+                            border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        )),
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        child: Text(
+                          'R\$ ${tr.value.toStringAsFixed(2)}', //INTERPOLAÇÃO
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tr.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            DateFormat('d MMM y')
+                                .format(tr.date), //importado do yaml
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
